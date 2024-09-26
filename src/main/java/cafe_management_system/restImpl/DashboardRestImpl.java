38e15ad71e8977d545a97b15f7693805bf96dd2c
MohammedFalih <yunusfalih123@gmail.com>
2024-09-26 17:57:48 +0530

    feat: dashboard API to get count of products, categories and bills

---
+++ src/main/java/cafe_management_system/restImpl/DashboardRestImpl.java
@@ -0,0 +1,23 @@
+package cafe_management_system.restImpl;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import cafe_management_system.rest.DashboardRest;
+import cafe_management_system.service.DashboardService;
+
+@RestController
+public class DashboardRestImpl implements DashboardRest {
+
+    @Autowired
+    DashboardService dashboardService;
+
+    @Override
+    public ResponseEntity<Map<String, Object>> getCount() {
+        return dashboardService.getCount();
+    }
+
+}
